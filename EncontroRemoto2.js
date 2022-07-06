6f37d7fa0db03143094f935a8f7f01644c642f0c
CaioLSDev <caio-l2@hotmail.com>
2022-07-06 14:47:33 -0300

    er2

---
+++ EncontroRemoto2.js
@@ -0,0 +1,24 @@
+//let numeroDeGeneros = 10
+let listaDeGeneros = ["Techno","Darkpsy","Hi-tech","Fullon","Goa"]
+//array
+
+for (let nome of listaDeGeneros) 
+{console.log ("o que está tocando agora é " + nome)}
+
+
+
+
+
+
+//anotaçoes variadas de estudo
+// for (let contador = 0; contador < numeroDeGeneros; contador++) {
+//    console.log (contador);
+//for (let contador = 0; contador < listaDeGeneros.length; contador++) {
+//    let alunoslistados = listaDeGeneros[1];
+//if (){}
+//else if (){}
+//else (){}
+//...+@+... concatenar 
+//'...${@}...' interpolar
+
+
